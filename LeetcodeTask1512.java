0c315326e57cd9ce359f92d8150dc6bb62ea0e79
NFilin <nickadm@yandex.ru>
2023-06-03 20:23:48 +0300

    Task 1512 solution

---
+++ LeetcodeTask1512.java
@@ -0,0 +1,28 @@
+public class LeetcodeTask1512 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 1, 1, 3};
+        int[] nums1 = new int[]{1, 1, 1, 1};
+        int[] nums2 = new int[]{1, 2, 3};
+        System.out.println(numIdenticalPairs(nums));
+        System.out.println(numIdenticalPairs(nums1));
+        System.out.println(numIdenticalPairs(nums2));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) count++;
+            }
+        }
+        return count;
+    }
+}
+
+/*
+1512. Number of Good Pairs
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+*/
