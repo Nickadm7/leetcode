83bb10dc45b8ea5f367eb5c9a14a17000d9409ab
NFilin <nickadm@yandex.ru>
2023-06-07 13:08:38 +0300

    Task 412 solution

---
+++ src/main/java/LeetcodeTask412.java
@@ -0,0 +1,45 @@
+package src.main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetcodeTask412 {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(3));
+        System.out.println(fizzBuzz(5));
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> out = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                out.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                out.add("Fizz");
+            } else if (i % 5 == 0) {
+                out.add("Buzz");
+            } else {
+                out.add(String.valueOf(i));
+            }
+        }
+        return out;
+    }
+}
+
+
+
+
+
+
+
+/*
+412. Fizz Buzz
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+*/
