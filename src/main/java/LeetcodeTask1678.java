6a7a101f56222c6a687e281c40a2ab2e15078faa
NFilin <nickadm@yandex.ru>
2023-06-08 23:37:24 +0300

    Task 1678 solution

---
+++ src/main/java/LeetcodeTask1678.java
@@ -0,0 +1,37 @@
+package src.main.java;
+
+public class LeetcodeTask1678 {
+    public static void main(String[] args) {
+        System.out.println(interpret("G()(al)"));
+        System.out.println(interpret("G()()()()(al)"));
+        System.out.println(interpret("(al)G(al)()()G"));
+    }
+
+    public static String interpret(String command) {
+        char[] input = command.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 'G') {
+                sb.append('G');
+            }
+            if (input[i] == '(' && input[i + 1] == ')') {
+                sb.append('o');
+            }
+            if (input[i] == '(' && input[i + 1] == 'a') {
+                sb.append("al");
+            }
+        }
+        return sb.toString();
+    }
+}
+
+/*
+1678. Goal Parser Interpretation
+
+You own a Goal Parser that can interpret a string command.
+The command consists of an alphabet of "G", "()" and/or "(al)" in some order.
+The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al".
+The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+*/
